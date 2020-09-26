93cf9664a9ebb95deb344172fedf9da70626c3be
Justice Payne <spikepayne@yahoo.com>
2020-09-26 22:19:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,70 +1,63 @@
 class Owner
-  @@all= []
-  attr_accessor :name, :pets
-  attr_reader :species
-
-  def initialize(species)
+  attr_accessor 
+  attr_writer 
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name, species = "human")
+    @name = name 
     @species = species
-    @@all << self
-    @pets = {:dogs => [], :cats => []}
-  end
+    @@all << self 
 
+  end
+  
   def say_species
-    return "I am a #{@species}."
+      "I am a #{self.species}." 
+  end 
+  
+  def self.all 
+    @@all 
   end
-
- def buy_dog(name_of_dog)
-    @pets[:dogs] << Dog.new(name_of_dog)
-  end
-
-  def buy_cat(name_of_cat)
-    @pets[:cats] << Cat.new(name_of_cat)
-  end
-
-  def walk_dogs
-    @pets.collect do |species, instances|
-      if species == :dogs
-        instances.each do |dog|
-          dog.mood = "happy"
-        end
-      end
-    end
-  end
-
-  def play_with_cats
-    @pets.collect do |species, instances|
-      if species == :cats
-        instances.each do |cat|
-          cat.mood = "happy"
-        end
-      end
-    end
-  end
-
-  def sell_pets
-    @pets.collect do |species, instances|
-      instances.each do |pet|
-        pet.mood = "nervous"
-      end
-      instances.clear
-    end
-  end
-
-  def list_pets
-    num_dogs = @pets[:dogs].size
-    num_cats = @pets[:cats].size
-    return "I have #{num_dogs} dog(s), and #{num_cats} cat(s)."
-  end
-
- def self.all
-    @@all
-  end
-
-  def self.reset_all
-    @@all.clear
-  end
-
+  
   def self.count
-    @@all.size
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear 
+  end
+  
+  def cats 
+    Cat.all.select {|cat| cat.owner == self}
+  end 
+  
+  def dogs  
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(cat)
+   Cat.new(cat, self) 
+  end
+  
+  def buy_dog(dog)
+   Dog.new(dog, self) 
+  end
+  
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+  
+  def sell_pets
+    self.dogs.each {|dog| dog.mood = "nervous"} && self.cats.each {|cat| cat.mood = "nervous"}
+    self.dogs.each {|dog| dog.owner = nil} && self.cats.each {|cat| cat.owner = nil}
+  end
+  
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 end
