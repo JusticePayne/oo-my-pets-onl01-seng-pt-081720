64bcaff5238996d41cef56bcf8dcd9faf95326a9
Justice Payne <spikepayne@yahoo.com>
2020-09-26 22:20:57 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,19 @@
 class Cat
+  attr_accessor :owner, :mood
+  attr_writer 
   attr_reader :name
-  attr_accessor :mood
-
-  def initialize(name)
-    @name = name
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
     @mood = "nervous"
+    @@all << self 
   end
+  
+  def self.all 
+    @@all 
+  end
+  
 end
