757f50e808f28b085938768fc84596210c436a71
Justice Payne <spikepayne@yahoo.com>
2020-09-26 22:21:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,19 @@
 class Dog
+  attr_accessor :owner, :mood
+  attr_writer 
   attr_reader :name
-  attr_accessor :mood
-
-  def initialize(name)
-    @name = name
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
     @mood = "nervous"
+    @@all << self 
   end
-
+  
+  def self.all 
+    @@all 
+  end
+  
 end
